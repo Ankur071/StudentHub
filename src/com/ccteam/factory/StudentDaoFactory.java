b66052c1b4bafbae73fc054ade3fa9d65e9f3034
Ankur071 <ankuryadav7559@gmail.com>
2024-07-24 10:29:59 +0530

    Add files via upload

---
+++ src/com/ccteam/factory/StudentDaoFactory.java
@@ -0,0 +1,18 @@
+package com.ccteam.factory;
+
+import com.ccteam.dao.StudentDao;
+import com.ccteam.dao.StudentDaoImpl;
+
+public class StudentDaoFactory 
+{
+	private static StudentDao studentDao;
+	static
+	{
+		studentDao = new StudentDaoImpl();
+	}
+	public static StudentDao getStudentDao()
+	{
+		return studentDao;
+	}
+
+}
---
+++ src/com/ccteam/factory/StudentServiceFactory.java
@@ -0,0 +1,18 @@
+package com.ccteam.factory;
+
+import com.ccteam.service.StudentService;
+import com.ccteam.service.StudentServiceImpl;
+
+public class StudentServiceFactory 
+{
+	private static StudentService studentService;
+	static 
+    {
+       studentService = new StudentServiceImpl();
+    }
+    public static StudentService getStudentService() 
+    {
+    return studentService;
+    }
+
+}
